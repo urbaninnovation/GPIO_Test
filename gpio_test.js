050238d92178c7bcf4308ade1a6feae0eb8adc4e
urbaninnovation <nils.schrum@gmx.de>
2021-03-22 13:12:16 +0100

    Zähler eingebaut

--- gpio_test.js
+++ gpio_test.js
@@ -1,8 +1,10 @@
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
 var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 var blinkInterval = setInterval(blinkLED, 1000); //run the blinkLED function every 250ms
-
+var int count =1
 function blinkLED() { //function to start blinking
+  count ++;
+  console.log(count);
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
     LED.writeSync(1); //set pin state to 1 (turn LED on)
   } else {
