77a7c1390459936ce3289bdf947a83f5c3258214
urbaninnovation <nils.schrum@gmx.de>
2021-03-22 14:41:43 +0100

    druckschalter aktiviert

--- re.js
+++ re.js
@@ -3,8 +3,9 @@
 	 https://www.youtube.com/watch?v=hFpSwfKw5G0
 */
 var Gpio = require('onoff').Gpio
-   ,button1 = new Gpio(6, 'in', 'both') //blue button
-   ,button2 = new Gpio(13, 'in', 'both') //red button
+   ,button1 = new Gpio(6, 'in', 'both') //DA button
+   ,button2 = new Gpio(13, 'in', 'both') //clk button
+   ,button3 = new Gpio(16, 'in', 'both') //druecken
    ,a = 0 //a value GPIO 17
    ,b = 0 //b value GPIO 18
 	 ,v = 0 //value to increment/decrement
@@ -29,6 +30,7 @@ button2.watch(function (err, value) {
     throw err;
   }
 	b = value;
+  console.log("Schalter 2")
 
 	//only evaluate if a = 1
 	if (a == 1 && b == 1) {
@@ -36,6 +38,15 @@ button2.watch(function (err, value) {
 	} else if (a==1 && b==0) {
 		console.log(v--);
 	}
+});
+
+//Watch for hardware interrupt of switch 1
+button3.watch(function (err, value) {
+  if (err) {
+    throw err;
+  }
+  //a = value;
+  console.log("Schalter gedrueckt!")
 });
 
 process.on('SIGINT', exit);
