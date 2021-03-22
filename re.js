a2cd169007c9c6c9a4e2e311bcc381ab4e181fef
urbaninnovation <nils.schrum@gmx.de>
2021-03-22 21:01:51 +0100

    new

--- re.js
+++ re.js
@@ -1,11 +1,6 @@
-/* This reads a rotary encoder with node.js
-   The idea for the logic came from the
-	 https://www.youtube.com/watch?v=hFpSwfKw5G0
-*/
 var Gpio = require('onoff').Gpio
-   ,button1 = new Gpio(23, 'in', 'both') //DA button
-   ,button2 = new Gpio(24, 'in', 'both') //clk button
-   ,button3 = new Gpio(16, 'in', 'both') //druecken
+   ,button1 = new Gpio(23, 'in', 'both') //blue button
+   ,button2 = new Gpio(24, 'in', 'both') //red button
    ,a = 0 //a value GPIO 17
    ,b = 0 //b value GPIO 18
 	 ,v = 0 //value to increment/decrement
@@ -21,7 +16,6 @@ button1.watch(function (err, value) {
     throw err;
   }
   a = value;
-  console.log("Schalter 1")
 });
 
 //Watch for hardware interrupt of switch 2
@@ -30,7 +24,6 @@ button2.watch(function (err, value) {
     throw err;
   }
 	b = value;
-  console.log("Schalter 2")
 
 	//only evaluate if a = 1
 	if (a == 1 && b == 1) {
@@ -39,6 +32,5 @@ button2.watch(function (err, value) {
 		console.log(v--);
 	}
 });
-
 
 process.on('SIGINT', exit);
