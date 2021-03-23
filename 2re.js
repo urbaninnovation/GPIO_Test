76b1ee965c33952ae8a581d8b9922c839cd5814b
urbaninnovation <nils.schrum@gmx.de>
2021-03-23 21:27:20 +0100

    nur neue werte drucken

--- 2re.js
+++ 2re.js
@@ -1,6 +1,6 @@
 const nodaryEncoder = require('nodary-encoder');
 const myEncoder = nodaryEncoder(23, 24); // Using GPIO17 & GPIO18
-
+var value_alt
 function exit() {
   //button1.unexport();
   //button2.unexport();
@@ -8,8 +8,10 @@ function exit() {
 }
 
 myEncoder.on('rotation', (direction, value) => {
-
+if (value != value_alt){
   console.log('Value is', value);
+  value_alt=value;
+  }
 });
 
 process.on('SIGINT', exit);
