09d6762227de5c1ac85491f044b93c071b7690f6
urbaninnovation <nils.schrum@gmx.de>
2021-03-23 21:20:03 +0100

    bug

--- 2re.js
+++ 2re.js
@@ -1,6 +1,12 @@
 const nodaryEncoder = require('nodary-encoder');
 const myEncoder = nodaryEncoder(23, 24); // Using GPIO17 & GPIO18
 
+function exit() {
+  button1.unexport();
+  button2.unexport();
+  process.exit();
+}
+
 myEncoder.on('rotation', (direction, value) => {
   if (direction == 'R') {
     console.log('Encoder rotated right');
