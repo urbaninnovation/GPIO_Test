63f02d222c98dbb2414c92bd1af29343407890ef
urbaninnovation <nils.schrum@gmx.de>
2021-03-23 10:26:31 +0100

    pins getauscht

--- re.js
+++ re.js
@@ -1,6 +1,6 @@
 var Gpio = require('onoff').Gpio
-   ,button1 = new Gpio(23, 'in', 'both') //blue button
-   ,button2 = new Gpio(24, 'in', 'both') //red button
+   ,button1 = new Gpio(24, 'in', 'both') //blue button
+   ,button2 = new Gpio(23, 'in', 'both') //red button
    ,a = 0 //a value GPIO 17
    ,b = 0 //b value GPIO 18
 	 ,v = 0 //value to increment/decrement
