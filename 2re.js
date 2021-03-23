97ed5cf805e8cad7315945fe16ab6be352efa8a9
urbaninnovation <nils.schrum@gmx.de>
2021-03-23 21:23:46 +0100

    s

--- 2re.js
+++ 2re.js
@@ -2,8 +2,8 @@ const nodaryEncoder = require('nodary-encoder');
 const myEncoder = nodaryEncoder(23, 24); // Using GPIO17 & GPIO18
 
 function exit() {
-  button1.unexport();
-  button2.unexport();
+  //button1.unexport();
+  //button2.unexport();
   process.exit();
 }
 
