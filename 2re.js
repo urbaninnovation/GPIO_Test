c60e9acca924c318c0b28b3bffeb31f1c0d0b30d
urbaninnovation <nils.schrum@gmx.de>
2021-03-27 20:42:41 +0100

    Test

--- 2re.js
+++ 2re.js
@@ -1,18 +1,20 @@
 const nodaryEncoder = require('nodary-encoder');
-const myEncoder = nodaryEncoder(23, 24); // Using GPIO17 & GPIO18
-var value_alt
+const myEncoder = nodaryEncoder(23, 24); // orginal hat  GPIO17 & GPIO18 genutzt
+var value_alt;
+var value = 4;
+
 console.log('zwei Click und der Zähler geht einen hoch oder herunter. ');
 
 function exit() {
-  //button1.unexport();
-  //button2.unexport();
   process.exit();
 }
+
+
 
 myEncoder.on('rotation', (direction, value) => {
 if (value != value_alt){
   console.log('Wert ist: ', value);
-  value_alt=value;
+  value_alt = value;
   }
 });
 
